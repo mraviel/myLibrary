bb6c84767a8642cbf490985635c81af14fb65520
mraviel <mraviel@outlook.com>
2021-08-12 12:33:03 +0300

    Redesign the database to have more than one table, The tables are: Users, Books, WishList, BooksRead.

--- Database/DatabaseFile.py
+++ Database/DatabaseFile.py
@@ -25,10 +25,24 @@ class Database:
     def create_table(self):
         """ Create a table to the database (Only once) """
         try:
-            self.cur.execute('''CREATE TABLE USERS (UserID int NOT NULL PRIMARY KEY,
+            self.cur.execute('''CREATE TABLE Users (UserID int NOT NULL PRIMARY KEY,
                                                     USERNAME varchar(255) NOT NULL UNIQUE CHECK(LENGTH(USERNAME)<=30),
                                                     PASSWORD varchar(255) NOT NULL,
                                                     EMAIl varchar(255) NOT NULL UNIQUE)''')
+
+            self.cur.execute('''CREATE TABLE Books (BookID int NOT NULL PRIMARY KEY,
+                                                    Name varchar(255) NOT NULL UNIQUE,
+                                                    Author varchar(255) NOT NULL,
+                                                    Description TEXT,
+                                                    Image varchar(255)''')
+
+            self.cur.execute('''CREATE TABLE WishList (ID int NOT NULL PRIMARY KEY,
+                                                           UserID int NOT NULL FOREIGN KEY,
+                                                           BookID int NOT NULL FOREIGN KEY)''')
+
+            self.cur.execute('''CREATE TABLE BooksRead (ID int NOT NULL PRIMARY KEY,
+                                                           UserID int NOT NULL FOREIGN KEY,
+                                                           BookID int NOT NULL FOREIGN KEY)''')
 
         except sqlite3.OperationalError as e:
             print("Table already exits ", e)
@@ -38,14 +52,14 @@ class Database:
         count_id = 1
         try:
             # Keep track of UserID column.
-            i = self.cur.execute('''SELECT * FROM USERS ORDER BY UserID DESC LIMIT 1''')
+            i = self.cur.execute('''SELECT * FROM Users ORDER BY UserID DESC LIMIT 1''')
             count_id = i.fetchall()[0][0] + 1
 
         except sqlite3.IntegrityError and IndexError:
             print("BIG PROBLEM...")
 
         # Insert the values to the database.
-        new_user = '''INSERT INTO USERS VALUES ({0}, "{1}", "{2}", "{3}")'''.format(count_id, user[0], user[1], user[2])
+        new_user = '''INSERT INTO Users VALUES ({0}, "{1}", "{2}", "{3}")'''.format(count_id, user[0], user[1], user[2])
         print(new_user)
         try:
             self.cur.execute(new_user)
@@ -55,12 +69,12 @@ class Database:
         self.conn.commit()  # Commit the changes.
 
         # Print the Users column.
-        for row in self.cur.execute('SELECT * FROM USERS ORDER BY UserID'):
+        for row in self.cur.execute('SELECT * FROM Users ORDER BY UserID'):
             print(row)
 
     def login(self, user):
         """ Log user to the database """
-        find_user = ''' SELECT * FROM USERS WHERE USERNAME="{0}" AND PASSWORD="{1}"; '''.format(user[0], user[1])
+        find_user = ''' SELECT * FROM Users WHERE USERNAME="{0}" AND PASSWORD="{1}"; '''.format(user[0], user[1])
         if self.cur.execute(find_user).fetchone():
             print("FOUND")
             return True
